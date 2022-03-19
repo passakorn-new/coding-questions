6b5dc314d94073dfd5926cefd4d9bff6aedd0254
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-03-19 18:51:05 +0700

    Time: 365 ms (25.00%), Space: 216.9 MB (100.00%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.rb
@@ -0,0 +1,26 @@
+class FreqStack
+    def initialize()
+        @hash_map = Hash.new { |h, k| h[k] = [] }
+        @freq = Hash.new(0)
+        @max_freq = 0
+    end
+
+    def push(val)
+        @freq[val] += 1
+        @max_freq = [@max_freq, @freq[val]].max
+        @hash_map[@freq[val]] << val
+    end
+
+    def pop()
+        remove_item = @hash_map[@max_freq].pop
+        @freq[remove_item] -= 1
+        @max_freq -= 1 if @hash_map[@max_freq].size == 0
+
+        remove_item
+    end
+end
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack.new()
+# obj.push(val)
+# param_2 = obj.pop()
