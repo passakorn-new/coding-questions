a3d4c7a040ccc84c9e51b17ed4336a0f4f0ffbb1
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-03-20 21:30:45 +0700

    Time: 1479 ms (50.00%), Space: 15.1 MB (77.02%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        list_size = len(tops)
+        freq_tops = [0] * 7
+        freq_bottoms = [0] * 7
+        equal_indexes = [0] * 7
+
+        for index in range(list_size):
+            freq_tops[tops[index]] += 1
+            freq_bottoms[bottoms[index]] += 1   
+            
+            if tops[index] == bottoms[index]:
+                equal_indexes[tops[index]] += 1
+
+        for index in range(7):
+            if freq_tops[index] + freq_bottoms[index] - equal_indexes[index] == list_size:
+                return list_size - max(freq_tops[index], freq_bottoms[index])
+        return  -1
+            
