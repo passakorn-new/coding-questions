c0b87a04f5fd98d50e8aa573203415630a3950a7
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-03-24 14:18:01 +0700

    Time: 630 ms (45.84%), Space: 20.2 MB (98.06%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -3,7 +3,17 @@ class Solution:
         # minimum boat = each boat should carry almost equal limit
         # implement left, right for use two pointer for group people
         count_boat, left, right = 0, 0, len(people) - 1 
-        people.sort() # can improve if use count sort
+        # people.sort() # can improve if use count sort
+        
+        index, count = 0, [0] * (limit + 1)
+        for p in people:
+            count[p] += 1
+            
+        for val in range(1, limit + 1):
+            while count[val] > 0:
+                people[index] = val
+                index += 1
+                count[val] -= 1
         
         while left <= right:            
             if people[left] + people[right] <= limit:
@@ -15,3 +25,4 @@ class Solution:
             count_boat += 1
             
         return count_boat
+    
