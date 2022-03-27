f7067eb7f59d7b22a3302bd0ec9a28194599c925
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-03-27 20:31:38 +0700

    Time: 200 ms (21.00%), Space: 14.6 MB (18.28%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,11 +1,12 @@
 from queue import PriorityQueue
 
-class Solution:
+class Solution:    
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         q = PriorityQueue()
         
         for idx, m in enumerate(mat):
-            soldiers = sum(m)
+            # soldiers = sum(m)
+            soldiers = self.binary_search(m) + 1
             q.put((soldiers, idx))
 
         res = []
@@ -14,7 +15,20 @@ class Solution:
             k -= 1
         return res
     
+    def binary_search(self, m: List[int]) -> int:
+        l, r = 0, len(m) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if (m[mid] != 0):
+                l = mid + 1
+            else:
+                r = mid - 1
+                
+        return l
     
     # NOTES
     # - sum soldiers each row
     # - sort by soldier if same number of soldier consider index  (implement by priority queue)
+    # - can improve when sum soldiers by use binary search for find 1 last index equal sum of soldiers
