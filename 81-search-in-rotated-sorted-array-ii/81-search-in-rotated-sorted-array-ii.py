788d868abdc096e45bdcedb95a20c3dcb62e7f1d
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-03-29 00:30:42 +0700

    Time: 82 ms (40.08%), Space: 14.5 MB (25.03%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -6,26 +6,25 @@ class Solution:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return True
-            
+
             # case duplicate number
             if nums[left] == nums[mid] and nums[left] == nums[right]:
                 left += 1
                 right -= 1
-                
+
             # left sorted
             elif nums[left] <= nums[mid]:
                 if nums[left] <= target and nums[mid] > target:
                     right = mid - 1
                 else:
                     left = mid + 1
-                    
+
             # right sorted
             else:
-                if nums[mid] < target and nums[right] >= target:
+                if  nums[right] >= target and nums[mid] < target:
                     left = mid + 1
                 else:
                     right = mid - 1
-                
         return False
                     
         
