4d02c4c5639f895468a33c872a89dbc03240fcfd
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-03-29 00:51:07 +0700

    Time: 99 ms (18.13%), Space: 14.3 MB (95.32%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -12,15 +12,19 @@ class Solution:
                 left += 1
                 right -= 1
 
-            # left sorted
+            # first half is in order
             elif nums[left] <= nums[mid]:
+                
+                # target is in first  half
                 if nums[left] <= target and nums[mid] > target:
                     right = mid - 1
                 else:
                     left = mid + 1
 
-            # right sorted
+            # second half is order
             else:
+                
+                # target is in second half
                 if  nums[right] >= target and nums[mid] < target:
                     left = mid + 1
                 else:
