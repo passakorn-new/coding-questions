89440c381a9d97ff1c21c5c7b40362b3fbed3670
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-04-07 01:52:44 +0700

    Time: 159 ms (100.00%), Space: 211.3 MB (100.00%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.rb
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.rb
@@ -1,14 +1,34 @@
-class Solution:
-    def threeSumMulti(self, arr: List[int], target: int) -> int:
-        mod = (10 ** 9) + 7
-        dict_map = {}
-        res = 0
-        
-        for i in range(0, len(arr)):
-            res = (res + dict_map.get(target - arr[i], 0)) % mod
+MOD = 10**9 + 7
+
+# @param {Integer[]} arr
+# @param {Integer} target
+# @return {Integer}
+def three_sum_multi(arr, target)
+    res = 0
+    freq_nums = Array.new(101, 0)
+    arr.sort!
+    
+    arr.each { |num| freq_nums[num] += 1 } 
+    
+    (0..100).step do |i|
+        (i..100).step do |j|
+            # k is remain value to equal target
+            k = target - i - j
             
-            for j in range(0, i):
-                two_sum = arr[i] + arr[j];
-                dict_map[two_sum] = dict_map.get(two_sum, 0) + 1
-        
-        return res
+            # next if k is impossible value  (0 <= arr[i] <= 100)
+            next if k < 0 || k > 100
+            
+            if i == j && j == k
+                res += (freq_nums[i] * (freq_nums[i]-1) * (freq_nums[i]-2)) /6;
+            elsif i == j && j != k
+                res += ((freq_nums[i] * (freq_nums[i]-1)) / 2) * freq_nums[k];
+            elsif i < j && j < k
+                res += freq_nums[i] * freq_nums[j] * freq_nums[k]
+            end
+            
+            res %= 1000000007
+        end
+    end
+    
+    res
+end
