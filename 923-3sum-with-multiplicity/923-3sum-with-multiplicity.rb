bc0e3542149c9d8cb72d5d186b390fa9998071ec
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-04-07 01:21:21 +0700

    Time: 7841 ms (5.81%), Space: 13.9 MB (92.44%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.rb
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.rb
@@ -1,46 +1,14 @@
-MOD = 10**9 + 7
-
-# @param {Integer[]} arr
-# @param {Integer} target
-# @return {Integer}
-def three_sum_multi(arr, target)
-  result = 0
-  arr.sort!
-
-  arr.count.times do |i|
-    t = target - arr[i]
-    j = i + 1
-    k = arr.count - 1
-
-    while j < k
-      if arr[j] + arr[k] < t
-        j += 1
-      elsif arr[j] + arr[k] > t
-        k -= 1
-      elsif arr[j] != arr[k]
-        left = right = 1
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        mod = (10 ** 9) + 7
+        dict_map = {}
+        res = 0
         
-        while j + 1 < k && arr[j] == arr[j+1]
-          left += 1
-          j += 1
-        end
+        for i in range(0, len(arr)):
+            res = (res + dict_map.get(target - arr[i], 0)) % mod
+            
+            for j in range(0, i):
+                two_sum = arr[i] + arr[j];
+                dict_map[two_sum] = dict_map.get(two_sum, 0) + 1
         
-        while k - 1 > j && arr[k] == arr[k-1]
-          right += 1
-          k -= 1
-        end
-
-        result += left*right
-        result %= MOD
-        j += 1
-        k -= 1
-      else
-        result += (k - j + 1)*(k - j)/2
-        result %= MOD
-        break
-      end
-    end
-  end
-  
-  result
-end
+        return res
