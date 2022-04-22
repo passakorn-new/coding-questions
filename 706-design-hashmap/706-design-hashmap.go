9fc621a3c5dc17d541c9d35f86418a8a757d39af
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-04-22 21:38:50 +0700

    Time: 133 ms (70.44%), Space: 9.2 MB (26.28%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.go
+++ 706-design-hashmap/706-design-hashmap.go
@@ -1,29 +1,89 @@
+type Node struct {
+    key int
+    value int
+    next *Node
+}
+
 type MyHashMap struct {
-    Size int
-    Hashmap [1e6][2]int
+    items []*Node
+    size int
 }
 
 func Constructor() MyHashMap {
-    var hashmap MyHashMap
-    hashmap.Size = 1e6
-    hashmap.Hashmap = [1e6][2]int{}
-    return hashmap
+    size := 1000
+    return MyHashMap {
+        make([]*Node, size),
+        size,
+    }
 }
 
 func (this *MyHashMap) Put(key int, value int)  {
-    hash := key % this.Size
-    this.Hashmap[hash] = [2]int{key, value}
+    index := key % this.size
+    item := this.items[index]
+    
+    if item == nil {
+        this.items[index] = &Node{key: key, value: value}
+        return
+    }
+    
+    for item != nil {
+        if item.key == key {
+            item.value = value
+            return
+        }
+        
+        if item.next == nil {
+            item.next = &Node{key: key, value: value}
+            return
+        }
+        
+        item = item.next
+    }
 }
 
 func (this *MyHashMap) Get(key int) int {
-    hash := key % this.Size
-    if this.Hashmap[hash] == [2]int{} {
+    index := key % this.size
+    if this.items[index] == nil {
         return -1
-    } 
-    return this.Hashmap[hash][1]
+    }
+    
+    item := this.items[index]
+    
+    for item != nil {
+        if item.key == key {
+            return item.value
+        }
+        
+        item = item.next
+    }
+    
+    return -1
 }
 
+
+/** Removes the mapping of the specified value key if this map contains a mapping for the key */
 func (this *MyHashMap) Remove(key int)  {
-    hash := key % this.Size
-    this.Hashmap[hash] = [2]int{}
+    index := key % this.size
+    if this.items[index] == nil {
+        return
+    }
+    
+    item := this.items[index]
+    
+    if item.key == key {
+        this.items[index] = item.next
+        return
+    }
+    
+    prev, item := item, item.next
+    
+    for item != nil {
+        if item.key == key {
+            prev.next = item.next
+            return
+        }
+        
+        prev = item        
+        item = item.next
+    }
 }
