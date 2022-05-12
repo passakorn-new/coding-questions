c4c6b8b7355735f582c884cabac365ae795cc87c
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-05-13 01:30:39 +0700

    Time: 4 ms (78.18%), Space: 4 MB (52.44%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.go
@@ -0,0 +1,31 @@
+func permuteUnique(nums []int) [][]int {
+    res := [][]int{}
+    perm := []int{}
+    count := make(map[int]int, 0)
+    
+    for _, n := range nums {
+        count[n]++
+    }
+    
+    backtracking(&res, perm, nums, count)
+    return res
+}
+
+func backtracking(res *[][]int, perm []int, nums []int, count map[int]int)  {
+    if len(perm) == len(nums) {
+        temp := make([]int, len(perm))
+        copy(temp, perm)
+        *res = append(*res, temp)
+        return
+    }
+    
+    for n, v := range count {
+        if v > 0 {
+            perm = append(perm, n)
+            count[n]--
+            backtracking(res, perm, nums, count)
+            perm = perm[:len(perm)-1]
+            count[n]++
+        }
+    }    
+}
