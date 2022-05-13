6247947ec265612fb80086ba7c5f4b043337655d
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-05-13 22:00:10 +0700

    Time: 5 ms (51.82%), Space: 6.5 MB (43.43%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+    if root == nil {
+        return nil
+    }
+    
+    queue := make([]*Node, 0) //BFS queue
+    queue = append(queue, root)
+    
+    for len(queue) > 0 {
+        l := len(queue) 
+        
+        for i := 0; i < l; i++ {  //iterate the length of queue only
+            node := queue[0]
+            queue = queue[1:]
+            
+            if i < l - 1 {
+                node.Next = queue[0] //not the last node in this layer
+            } else if i == l - 1 {
+                node.Next = nil //the last node in this layer
+            }
+            
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+        }
+    }
+
+    return root
+}
