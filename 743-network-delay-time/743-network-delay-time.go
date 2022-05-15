15647163b351aae9681d2a8165200c856efafbf5
passakorn.new <52862280+passakorn-new@users.noreply.github.com>
2022-05-15 16:55:16 +0700

    Time: 115 ms (33.96%), Space: 8 MB (34.59%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.go
+++ 743-network-delay-time/743-network-delay-time.go
@@ -1,4 +1,3 @@
-// review
 type Distance struct {
     W int
     V int
@@ -21,34 +20,42 @@ func (h *DistanceHeap) Pop() interface{} {
 }
 
 func networkDelayTime(times [][]int, N int, K int) int {
-    graph := map[int][]Distance{}
+    graph := map[int]DistanceHeap{}
     
+    // keep data node in graph connected other node anything
+    // graph[2] = [{ W: 1, V: 3 }, {W: 1, V: 1}]  -> node 2 connected node 3 weight 1 and node 1 weight 1
     for _, time := range times {
         u, v, w := time[0], time[1], time[2]
         graph[u] = append(graph[u], Distance{w, v})
     }
     
+    // initial distance start node 2 weight0
     q := &DistanceHeap{
         Distance{0, K},
     }
     
     arrivalTime := map[int]int{}
     
+    // bfs process
     for q.Len() > 0 {
+        // pop bfs queue
         d := heap.Pop(q).(Distance)
-        
+
         if _, ok := arrivalTime[d.V]; ok {
             continue
         }
         
+        // update time to arrival
         arrivalTime[d.V] = d.W
         
+        // update next queue and accumulate time
         for _, next := range graph[d.V] {
             next.W += d.W
             heap.Push(q, next)
         }
     }
     
+    // visit not complete all nodes
     if len(arrivalTime) < N {
         return -1
     }
